1db0ad7f1625874a9a767b2401f7c0375eda79fe
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2023-05-29 15:54:47 -0300

    Adiciona classe base para uso do ReportPortal

---
+++ Utils/Report.cs
@@ -0,0 +1,16 @@
+﻿using ReportPortal.Shared;
+
+namespace TestPetStore.Utils
+{
+    public class Report
+    {
+        public Report() 
+        { 
+        }
+
+        public void Mensagem(string msg)
+        {
+            Context.Current.Log.Info(msg);
+        }
+    }
+}
