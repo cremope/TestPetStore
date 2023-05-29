99936a5517276e13667cb7a34fff4b49e17f63d9
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2023-05-29 15:55:39 -0300

    Adiciona classe de inicialização do projeto em modo estatico

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TestPetStore
+{
+    public static class Startup
+    {
+        public static IConfiguration APPSETTINGS { get; }
+
+        static Startup()
+        {
+            APPSETTINGS = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json")
+                .Build();
+        }
+    }
+}
