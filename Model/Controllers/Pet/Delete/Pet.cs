b6c3b9182913cbdd11cb269d30803dd61e97bfe9
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2023-05-29 15:51:46 -0300

    Adiciona model de reponse do método delete

---
+++ Model/Controllers/Pet/Delete/Pet.cs
@@ -0,0 +1,9 @@
+﻿namespace TestPetStore.Model.Controllers.Pet.Delete
+{
+    public class ResponseDeletePet
+    {
+        public int code { get; set; }
+        public string type { get; set; }
+        public string message { get; set; }
+    }
+}
